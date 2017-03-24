90985f98e4403ac3fda8dd43e8502724ae64aa64
AdamRobertsky <robertadamski96@gmail.com>
2017-03-24 23:41:04 +0100

    Map

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public Material lava;
+    public Material earth;
+    int a;
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform child in transform)
+        {
+            a = Random.Range(0, 2);
+            if (a == 0)
+            {
+                child.GetComponent<MeshRenderer>().material = lava;
+                child.tag = "Lava";
+            }
+            if (a == 1)
+            {
+                child.GetComponent<MeshRenderer>().material = earth;
+                child.tag = "Earth";
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
