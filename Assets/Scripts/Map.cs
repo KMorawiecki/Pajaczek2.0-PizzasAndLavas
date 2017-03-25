d4fcf8d6cabeed935d642a7474f0e04e60a3bf8d
AdamRobertsky <robertadamski96@gmail.com>
2017-03-25 03:35:06 +0100

    Szajs

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map : MonoBehaviour {
+public class Map : MonoBehaviour
+{
 
     public Material lava;
     public Material earth;
+    public GameObject terrainPrefab;
     int a;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         foreach (Transform child in transform)
         {
             a = Random.Range(0, 2);
@@ -22,17 +25,20 @@ public class Map : MonoBehaviour {
             {
                 child.GetComponent<MeshRenderer>().material = earth;
                 child.tag = "Earth";
+                //var terrain = Instantiate(terrainPrefab, child.position, child.rotation, transform);
             }
-            if(child.transform.position == Vector3.zero)
+            if (child.transform.position == Vector3.zero)
             {
                 child.GetComponent<MeshRenderer>().material = earth;
                 child.tag = "Earth";
+                //Instantiate(terrainPrefab, child.position, child.rotation, transform);
             }
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
